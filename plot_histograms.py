8d01cef418dd24660fcb74505d274eab9c10255e
Erin Shockley <Erin.Shockley@Vanderbilt.edu>
2014-12-29 14:41:11 -0500

    Replacing sampled parameter distributions after changing which parameters were fixed -- needed to fix KD_AG_allo3, not KD_AG_allo2.  Small changes to PyMC sampling script and histogram plotting script.

--- plot_histograms.py
+++ plot_histograms.py
@@ -12,10 +12,10 @@ fig1, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2)
 
 fig2, ((ax7, ax8), (ax9, ax10), (ax11, ax12)) = plt.subplots(3, 2)
 
-trace = np.load('2014_12_20_Dream_COX2_parameter_fits.p')
+trace = np.load('2014_12_28_Dream_COX2_parameter_fits.p')
 
 for axis, variable in zip([ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10, ax11, ax12], trace):
-    axis.hist(trace[variable][0][40000::], bins=200)
+    axis.hist(trace[variable][0][100000::], bins=200)
     axis.set_title(str(variable))
     axis.set_xlabel('Log(parameter value)')
     axis.set_ylabel('Frequency')
--- run_pymc_sampling.py
+++ run_pymc_sampling.py
@@ -36,13 +36,13 @@ KD_AA_cat1 = np.log10(cox2_model.parameters['kr_AA_cat1'].value/cox2_model.param
 kcat_AA1 = np.log10(cox2_model.parameters['kcat_AA1'].value)
 KD_AG_cat1 = np.log10(cox2_model.parameters['kr_AG_cat1'].value/cox2_model.parameters['kf_AG_cat1'].value)
 kcat_AG1 = np.log10(cox2_model.parameters['kcat_AG1'].value)
-KD_AG_allo2 = np.log10(cox2_model.parameters['kr_AG_allo2'].value/cox2_model.parameters['kf_AG_allo2'].value)
+KD_AG_allo3 = np.log10(cox2_model.parameters['kr_AG_allo3'].value/cox2_model.parameters['kf_AG_allo3'].value)
 
 #Likelihood function to generate simulated data that corresponds to experimental time points
 @theano.compile.ops.as_op(itypes=[t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar, t.dscalar] \
 ,otypes=[t.dmatrix, t.dmatrix])
 def likelihood(KD_AA_cat2, kcat_AA2, KD_AA_cat3, kcat_AA3, \
-    KD_AG_cat2, KD_AG_cat3, kcat_AG3, KD_AA_allo1, KD_AA_allo2, KD_AA_allo3, KD_AG_allo1, KD_AG_allo3):
+    KD_AG_cat2, KD_AG_cat3, kcat_AG3, KD_AA_allo1, KD_AA_allo2, KD_AA_allo3, KD_AG_allo1, KD_AG_allo2):
     
 #    print 'KD_AA_cat2: ',KD_AA_cat2
 #    print 'kcat_AA2: ',kcat_AA2
@@ -86,10 +86,10 @@ def likelihood(KD_AA_cat2, kcat_AA2, KD_AA_cat3, kcat_AA3, \
     cox2_model.parameters['kr_AA_allo3'].value = 10**(KD_AA_allo3+generic_kf)
     cox2_model.parameters['kf_AG_allo1'].value = 10**generic_kf
     cox2_model.parameters['kr_AG_allo1'].value = 10**(KD_AG_allo1+generic_kf)
-#    cox2_model.parameters['kf_AG_allo2'].value = 10**generic_kf
-#    cox2_model.parameters['kr_AG_allo2'].value = 10**(KD_AG_allo2*generic_kf)
-    cox2_model.parameters['kf_AG_allo3'].value = 10**generic_kf
-    cox2_model.parameters['kr_AG_allo3'].value = 10**(KD_AG_allo3+generic_kf)
+#    cox2_model.parameters['kf_AG_allo3'].value = 10**generic_kf
+#    cox2_model.parameters['kr_AG_allo3'].value = 10**(KD_AG_allo2*generic_kf)
+    cox2_model.parameters['kf_AG_allo2'].value = 10**generic_kf
+    cox2_model.parameters['kr_AG_allo2'].value = 10**(KD_AG_allo2+generic_kf)
     
     PG_array = np.zeros((7,7), dtype='float64')
     PGG_array = np.zeros((7,7), dtype='float64')    
@@ -127,14 +127,14 @@ def likelihood_thermobox2(KD_AA_allo1, KD_AA_allo3, KD_AG_cat3):
     print 'box2: ',box2
     return np.array(box2, dtype='float64')
     
-@theano.compile.ops.as_op(itypes=[t.dscalar, t.dscalar], otypes=[t.dscalar])
-def likelihood_thermobox3(KD_AA_cat2, KD_AG_allo1):
+@theano.compile.ops.as_op(itypes=[t.dscalar, t.dscalar, t.dscalar], otypes=[t.dscalar])
+def likelihood_thermobox3(KD_AA_cat2, KD_AG_allo1, KD_AG_allo2):
     box3 = (1/(10**KD_AG_allo1))*(1/(10**KD_AA_cat2))*(10**KD_AG_allo2)*(10**KD_AA_cat1)
     print 'box3: ',box3
     return np.array(box3, dtype='float64')
 
-@theano.compile.ops.as_op(itypes=[t.dscalar, t.dscalar, t.dscalar], otypes=[t.dscalar])
-def likelihood_thermobox4(KD_AG_cat2, KD_AG_allo1, KD_AG_allo3):
+@theano.compile.ops.as_op(itypes=[t.dscalar, t.dscalar], otypes=[t.dscalar])
+def likelihood_thermobox4(KD_AG_cat2, KD_AG_allo1):
     box4 = (1/(10**KD_AG_cat1))*(1/(10**KD_AG_allo3))*(10**KD_AG_cat2)*(10**KD_AG_allo1)
     print 'box4: ',box4
     return np.array(box4, dtype='float64')
@@ -158,8 +158,8 @@ with model:
     pm.Normal('KD_AA_allo2', mu=np.log10(cox2_model.parameters['kr_AA_allo2'].value/cox2_model.parameters['kf_AA_allo2'].value), sd=1, dtype='float64')
     pm.Normal('KD_AA_allo3', mu=np.log10(cox2_model.parameters['kr_AA_allo3'].value/cox2_model.parameters['kf_AA_allo3'].value), sd=1, dtype='float64')
     pm.Normal('KD_AG_allo1', mu=np.log10(cox2_model.parameters['kr_AG_allo1'].value/cox2_model.parameters['kf_AG_allo1'].value), sd=1, dtype='float64')
-    #pm.Normal('KD_AG_allo2', mu=np.log10(cox2_model.parameters['kr_AG_allo2'].value/cox2_model.parameters['kf_AG_allo2'].value), sd=.08, dtype='float64')
-    pm.Normal('KD_AG_allo3', mu=np.log10(cox2_model.parameters['kr_AG_allo3'].value/cox2_model.parameters['kf_AG_allo3'].value), sd=1, dtype='float64')
+    pm.Normal('KD_AG_allo2', mu=np.log10(cox2_model.parameters['kr_AG_allo2'].value/cox2_model.parameters['kf_AG_allo2'].value), sd=1, dtype='float64')
+    #pm.Normal('KD_AG_allo3', mu=np.log10(cox2_model.parameters['kr_AG_allo3'].value/cox2_model.parameters['kf_AG_allo3'].value), sd=1, dtype='float64')
     
     #Set starting location for sampling
     start = {#model.KD_AA_cat1: np.log10(cox2_model.parameters['kr_AA_cat1'].value/cox2_model.parameters['kf_AA_cat1'].value), model.kcat_AA1: np.log10(cox2_model.parameters['kcat_AA1'].value),\
@@ -169,8 +169,8 @@ with model:
     model.KD_AG_cat2: np.log10(cox2_model.parameters['kr_AG_cat2'].value/cox2_model.parameters['kf_AG_cat2'].value), model.KD_AG_cat3: np.log10(cox2_model.parameters['kr_AG_cat3'].value/cox2_model.parameters['kf_AG_cat3'].value), \
     model.kcat_AG3: np.log10(cox2_model.parameters['kcat_AG3'].value), model.KD_AA_allo1: np.log10(cox2_model.parameters['kr_AA_allo1'].value/cox2_model.parameters['kf_AA_allo1'].value), \
     model.KD_AA_allo2: np.log10(cox2_model.parameters['kr_AA_allo2'].value/cox2_model.parameters['kf_AA_allo2'].value), model.KD_AA_allo3: np.log10(cox2_model.parameters['kr_AA_allo3'].value/cox2_model.parameters['kf_AA_allo3'].value), \
-    model.KD_AG_allo1: np.log10(cox2_model.parameters['kr_AG_allo1'].value/cox2_model.parameters['kf_AG_allo1'].value), #model.KD_AG_allo2: np.log10(cox2_model.parameters['kr_AG_allo2'].value/cox2_model.parameters['kf_AG_allo2'].value), \
-    model.KD_AG_allo3: np.log10(cox2_model.parameters['kr_AG_allo3'].value/cox2_model.parameters['kf_AG_allo3'].value)}        
+    model.KD_AG_allo1: np.log10(cox2_model.parameters['kr_AG_allo1'].value/cox2_model.parameters['kf_AG_allo1'].value), model.KD_AG_allo2: np.log10(cox2_model.parameters['kr_AG_allo2'].value/cox2_model.parameters['kf_AG_allo2'].value)} 
+    #model.KD_AG_allo3: np.log10(cox2_model.parameters['kr_AG_allo3'].value/cox2_model.parameters['kf_AG_allo3'].value)}        
     
     #Model likelihood - compare simulated values of PGs and PGGs at various substrate concentrations
     PG_output, PGG_output = likelihood(\
@@ -178,8 +178,8 @@ with model:
     model.KD_AA_cat2, model.kcat_AA2, model.KD_AA_cat3, model.kcat_AA3, \
     #model.KD_AG_cat1, model.kcat_AG1, 
     model.KD_AG_cat2, model.KD_AG_cat3, model.kcat_AG3, model.KD_AA_allo1, model.KD_AA_allo2, model.KD_AA_allo3, model.KD_AG_allo1, \
-    #model.KD_AG_allo2, 
-    model.KD_AG_allo3)
+    model.KD_AG_allo2) 
+    #model.KD_AG_allo3)
     
     pm.Normal('PGs_observed', mu=PG_output, sd=exp_data_sd_PG, observed=exp_data_PG)
     pm.Normal('PGGs_observed', mu=PGG_output, sd=exp_data_sd_PGG, observed=exp_data_PGG)
@@ -191,10 +191,10 @@ with model:
     box2 = likelihood_thermobox2(model.KD_AA_allo1, model.KD_AA_allo3, model.KD_AG_cat3)
     pm.Normal('thermodynamic_box2', mu=box2, sd=1e-2, observed=1)
     
-    box3 = likelihood_thermobox3(model.KD_AA_cat2, model.KD_AG_allo1)
+    box3 = likelihood_thermobox3(model.KD_AA_cat2, model.KD_AG_allo1, model.KD_AG_allo2)
     pm.Normal('thermodynamic_box3', mu=box3, sd=1e-2, observed=1)
     
-    box4 = likelihood_thermobox4(model.KD_AG_cat2, model.KD_AG_allo1, model.KD_AG_allo3)
+    box4 = likelihood_thermobox4(model.KD_AG_cat2, model.KD_AG_allo1)
     pm.Normal('thermodynamic_box4', mu=box4, sd=1e-2, observed=1)
     
     #Start from end of last trace
@@ -204,7 +204,7 @@ with model:
     step = pm.Dream(nseedchains=120, save_history=True, blocked=True)
     
     
-    trace = pm.sample(500000, step, njobs=5)
+    trace = pm.sample(250000, step, njobs=5)
     
     dictionary_to_pickle = {}
     print len(trace)
@@ -212,5 +212,5 @@ with model:
         for var in dictionary:
            dictionary_to_pickle[var] = trace[var] 
     
-    pickle.dump(dictionary_to_pickle, open('2014_12_20_Dream_COX2_parameter_fits.p', 'wb'))
+    pickle.dump(dictionary_to_pickle, open('2014_12_28_Dream_COX2_parameter_fits.p', 'wb'))
     
